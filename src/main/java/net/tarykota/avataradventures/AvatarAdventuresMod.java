be224ea34d0f58775fb58a2b14985a06b8203c1a
Tarykota <tomsam626@gmail.com>
2023-08-22 11:25:51 -0500

    Organizing

--- src/main/java/net/tarykota/avataradventures/AvatarAdventuresMod.java
+++ src/main/java/net/tarykota/avataradventures/AvatarAdventuresMod.java
@@ -18,10 +18,8 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(AvatarAdventuresMod.MOD_ID)
 public class AvatarAdventuresMod {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "avataradventures";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public AvatarAdventuresMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
