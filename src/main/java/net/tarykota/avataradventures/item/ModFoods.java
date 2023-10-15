a59f29a1506cb28f04fe12f1220df30999b96c16
Tarykota <tomsam626@gmail.com>
2023-10-14 19:11:06 -0500

    Tweaked Glow worm effects to include 30 seconds of nausea and a minute of slowness III

--- src/main/java/net/tarykota/avataradventures/item/ModFoods.java
+++ src/main/java/net/tarykota/avataradventures/item/ModFoods.java
@@ -8,5 +8,6 @@ public class ModFoods {
     public static final FoodProperties TEYLU = new FoodProperties.Builder().nutrition(2).saturationMod(0.3F).build();
     public static final FoodProperties COOKED_TEYLU = new FoodProperties.Builder().nutrition(6).saturationMod(0.8F).build();
     public static final FoodProperties GLOW_WORM = new FoodProperties.Builder().nutrition(2).saturationMod(0.3F)
-            .effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 300, 0), 1.0F).build();
+            .effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 600, 0), 1.0F)
+            .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 1200, 2), 1.0F).build();
 }
